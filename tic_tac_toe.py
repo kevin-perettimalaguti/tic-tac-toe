da0f7d2381aa2b732215a841a8198de99155bf53
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2023-12-13 10:34:53 +0100

    j2 tic tac toe

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,42 +1,13 @@
-# Initialise le plateau de jeu
-    # 1. Crée une variable pour représenter le plateau de jeu.
-    # 2. Choisis la taille du plateau (par exemple, 3x3) et initialise-le avec des valeurs par défaut.
-    # 3. Assure-toi que chaque case du plateau est vide au début du jeu.
-
-#///===FONCTIONS===\\\
-    
-    # Crée une fonction pour afficher le plateau de jeu
-        # 1. Définis une fonction qui affiche le plateau de jeu.
-        # 2. Utilise une boucle pour parcourir chaque ligne du plateau.
-        # 3. À l'intérieur de la boucle, utilise une autre boucle pour parcourir chaque case de la ligne.
-        # 4. Affiche le contenu de chaque case (X, O ou vide) avec des séparateurs.
-        
-    # Crée une fonction pour vérifier s'il y a un alignement gagnant
-        # 1. Crée une fonction qui prend le plateau en entrée et vérifie s'il y a un alignement gagnant.
-        # 2. Définis les conditions nécessaires pour qu'il y ait un alignement (horizontal, vertical, diagonal).
-        # 3. Renvoie le symbole du joueur gagnant s'il y en a un, sinon renvoie un symbole indiquant l'absence de gagnant.
-
-    # Crée une fonction pour vérifier si le plateau est plein
-        # 1. Crée une fonction qui prend le plateau en entrée et vérifie si toutes les cases sont remplies.
-        # 2. Renvoie une valeur indiquant si le plateau est plein ou non.
-
-
-#///===BOUCLE PRINCIPAL DU JEU===\\\
-    # Met en place la boucle principale du jeu
-    # À chaque tour, affiche le plateau, prend l'entrée du joueur, met à jour le plateau
-    
-# Vérifie s'il y a un gagnant ou si le plateau est plein
-
-
-#importation des modules pygame
+# Importation des modules pygame
 import pygame, pygame_menu
+import sys
 
 # Définition de couleurs utilisées dans le jeu
-BLACK= (0, 0, 0)
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE= (0, 0, 255)
+BLUE = (0, 0, 255)
 BLEU_CLAIR = (102, 255, 255)
 PURPLE = (182, 48, 243)
 GREY = (170, 166, 184)
@@ -49,7 +20,7 @@ fenetre_hauteur = 480
 fenetre = pygame.display.set_mode((fenetre_largeur, fenetre_hauteur))
 pygame.display.set_caption("Tic-Tac-Toe")
 
-#Initialisation du plateau du jeu
+# Initialisation du plateau du jeu
 largeur_plateau = 950
 hauteur_plateau = 650
 taille_plateau = largeur_plateau * hauteur_plateau
@@ -57,71 +28,80 @@ une_case = (largeur_plateau // 3 * hauteur_plateau // 3)
 
 # Fonction pour afficher le plateau de jeu
 def afficher_plateau():
-    fenetre.fill(WHITE) # Le fond de l'écran de jeu en blanc
-    
+    fenetre.fill(WHITE)  # Le fond de l'écran de jeu en blanc
+
     # Ma Grille en 3x3
-    plateau = [['X', 'O', 'X'],
-               ['O', ' ', 'O'],
-               ['X', 'O', 'X']]
-    
-    # Taille et position des cases
-    case_largeur, case_hauteur = une_case
-    x, y = 50, 50
-    
-    # Boucle pour parcourir chaque ligne du plateau
-    for ligne in plateau:
-        # Boucle pour parcourir chaque case de la ligne
-        for cases in ligne:
-            pygame.draw.rect(fenetre, BLACK, (x, y, case_largeur, case_hauteur), 2)
-            if cases != ' ':
+    for i in range(3):
+        for j in range(3):
+            pygame.draw.rect(fenetre, BLACK, (i * une_case[0], j * une_case[1], une_case[0], une_case[1]), 2)
+            if plateau[j][i] != ' ':
                 font = pygame.font.Font(None, 36)
-                texte = font.render(cases, True, BLACK)
-                fenetre.blit(texte, (x + case_largeur // 3, y + case_hauteur // 3))
-            
-            x += case_largeur
-        x = 50
-        y += case_hauteur
-        
-def verification_alignement_gagnant(jeu):
-    global taille_plateau
-    global une_case
-    
-    for v in taille_plateau:
-        if v in taille_plateau.count('X'):
-            return True
-        
-        if v in taille_plateau.count('O'):
-            return True
-        
-        
-    
-    
+                texte = font.render(plateau[j][i], True, BLACK)
+                fenetre.blit(texte, (i * une_case[0] + une_case[0] // 3, j * une_case[1] + une_case[1] // 3))
 
+def verification_alignement_gagnant():
+    # Vérification des lignes
+    for ligne in plateau:
+        if ligne.count('X') == 3:
+            return 'X'
+        elif ligne.count('O') == 3:
+            return 'O'
 
+    # Vérification des colonnes
+    for i in range(3):
+        if plateau[0][i] == plateau[1][i] == plateau[2][i] and plateau[0][i] != ' ':
+            return plateau[0][i]
 
+    # Vérification des diagonales
+    if plateau[0][0] == plateau[1][1] == plateau[2][2] and plateau[0][0] != ' ':
+        return plateau[0][0]
+    elif plateau[0][2] == plateau[1][1] == plateau[2][0] and plateau[0][2] != ' ':
+        return plateau[0][2]
 
+    return None
 
+#///===FONCTIONS===\\\
 
+# Crée une fonction pour initialiser le plateau de jeu
+def initialiser_plateau():
+    return [[' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']]
 
+# Crée une fonction pour vérifier si le plateau est plein
+def plateau_plein():
+    for ligne in plateau:
+        if ' ' in ligne:
+            return False
+    return True
 
+#///===BOUCLE PRINCIPAL DU JEU===\\\
 
+# Initialise le plateau de jeu
+plateau = initialiser_plateau()
 
+# Met en place la boucle principale du jeu
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
+    # Affiche le plateau de jeu
+    afficher_plateau()
 
+    # Vérification de l'alignement gagnant
+    gagnant = verification_alignement_gagnant()
+    if gagnant:
+        print(f'Le joueur {gagnant} a gagné!')
+        pygame.quit()
+        sys.exit()
 
+    # Vérification si le plateau est plein
+    if plateau_plein():
+        print("Match nul!")
+        pygame.quit()
+        sys.exit()
 
-
-# Initialisation du menu principal
-menu = pygame_menu.Menu('Bienvenue', 700, 480, theme=pygame_menu.themes.THEME_BLUE)
-menu.add.button('Jouer au Pendu')
-menu.add.button('Quitter', pygame_menu.events.EXIT)
-
-# Lancement du menu
-menu.mainloop(fenetre)
-
-# Fermeture de Pygame
-pygame.quit()
-
-
-
-
+    # Mise à jour de l'affichage
+    pygame.display.flip()
